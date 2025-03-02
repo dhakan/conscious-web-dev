3b46e439fa9fcb0931e867965c78f5e945a2e040
Dannie Håkansson <danniehakan@gmail.com>
2025-03-02 20:51:06 +0100

    Add form blur code

--- event-propagation/nested-clicks/index.ts
+++
@@ -1,8 +0,0 @@
-const listItemElement = document.querySelector("li");
-const buttonElement = listItemElement?.querySelector("button");
-
-buttonElement?.addEventListener("click", (event) => {
-  console.log("Button clicked!");
-  event.stopPropagation();
-  event.preventDefault();
-});
